f3f109b376139760bea59dd5ea67e8471c821339
damon5378 <iosifov.konstantin@gmail.com>
2020-01-23 21:24:12 +0300

    added react-js project

---
+++ 01/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import Nav from './components/Nav.jsx';
+import Profile from './components/Profile';
+
+
+const App = () => {
+  return (
+    <div className = 'app-wrapper'>
+      <Header />
+      <Nav />
+      <Profile />
+      </div>
+  );
+}
+
+export default App;
---
+++ 01/my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className = 'header'>
+        <img src = 'https://www.freeiconspng.com/uploads/flame-logo-png-30.png'></img>
+      </header>
+    );
+}
+
+export default Header;
